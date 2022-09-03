767bf4697a09900dafe520b1859bf54242b9958d
Zade Viggers <zade@viggers.net>
2022-09-04 08:51:30 +1200

    Add blastoff mode!

--- packages/astro-beep/index.js
+++ packages/astro-beep/index.js
@@ -8,48 +8,68 @@ import notifier from 'node-notifier';
 import escalade from 'escalade';
 
 function getTimeStat(timeStart, timeEnd) {
-  const buildTime = timeEnd - timeStart;
-  return buildTime < 750 ? `${Math.round(buildTime)}ms` : `${(buildTime / 1e3).toFixed(2)}s`;
+	const buildTime = timeEnd - timeStart;
+	return buildTime < 750 ? `${Math.round(buildTime)}ms` : `${(buildTime / 1e3).toFixed(2)}s`;
 }
 
 function asset(name) {
-    return fileURLToPath(new URL(`./lib/${name}`, import.meta.url))
+	return fileURLToPath(new URL(`./lib/${name}`, import.meta.url));
 }
 
-export default function beep() {
-    const isWin = platform() === 'win32';
-    
-    return {
-        name: 'astro-beep',
-        hooks: {
-            'astro:build:start': async () => {
-                const start = performance.now()
-                const root = pathToFileURL(await escalade(process.cwd(), (dir, names) => {
-                    if (names.includes('package.json')) {
-                        return 'package.json'
-                    }
-                }));
-                const pkg = new URL('./package.json', root);
-                const { name } = JSON.parse(readFileSync(pkg, 'utf-8'))
-                process.on('exit', (code) => {
-                    const time = getTimeStat(start, performance.now() + 10);
-                    if (code !== 0) {
-                        if (!isWin) exec(`afplay ${asset('error.aiff')}`);
-                        notifier.notify({
-                            title: `Astro: ${name}`,
-                            message: `❌ Build failed in ${time}`,
-                            icon: asset('astro.png')
-                        })
-                    } else {
-                        if (!isWin) exec(`afplay ${asset('success.aiff')}`);
-                        notifier.notify({
-                            title: `Astro: ${name}`,
-                            message: `✅ Build completed in ${time}`,
-                            icon: asset('astro.png')
-                        })
-                    }
-                })
-            }
-        }
-    }
+const assets = {
+	normal: {
+		successSound: asset('success.aiff'),
+		errorSound: asset('error.aiff'),
+		successLogo: asset('astro.png'),
+		errorLogo: asset('astro.png'),
+	},
+	blastoff: {
+		successSound: asset('success-blastoff.aiff'),
+		errorSound: asset('error-blastoff.aiff'),
+		successLogo: asset('astro.png'),
+		errorLogo: asset('astro.png'),
+	},
+};
+
+export { assets as assets };
+
+export default function beep(options) {
+	const mode = options?.mode || 'normal';
+	const isWin = platform() === 'win32';
+
+	return {
+		name: 'astro-beep',
+		hooks: {
+			'astro:build:start': async () => {
+				const start = performance.now();
+				const root = pathToFileURL(
+					await escalade(process.cwd(), (dir, names) => {
+						if (names.includes('package.json')) {
+							return 'package.json';
+						}
+					})
+				);
+				const pkg = new URL('./package.json', root);
+				const { name } = JSON.parse(readFileSync(pkg, 'utf-8'));
+				process.on('exit', (code) => {
+					const time = getTimeStat(start, performance.now() + 10);
+					if (code !== 0) {
+						if (!isWin) exec(`afplay ${assets[mode].errorSound}`);
+						notifier.notify({
+							title: `Astro: ${name}`,
+							message: `❌ Build failed in ${time}`,
+							icon: assets[mode].errorLogo,
+						});
+					} else {
+						if (!isWin) exec(`afplay ${assets[mode].successSound}`);
+						notifier.notify({
+							title: `Astro: ${name}`,
+							message: `✅ Build completed in ${time}`,
+							icon: assets[mode].successLogo,
+						});
+					}
+				});
+			},
+		},
+	};
 }
